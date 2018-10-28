1a289e8f107c5220dd8307e9eaba2255e86ced6f
Andrzej Jędrzejczak <jedrzejczak.andrzej89@gmail.com>
2018-10-28 20:38:50 +0100

    sample login page test

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -2,6 +2,11 @@ describe('The Home Page', function() {
     it('successfully loads', function() {
         cy.visit('/') // change URL to match your dev URL
 
-        cy.
+        cy.get('h1').should('contain', 'Cypress!');
+
+        cy.get('form.login').should('exist').and('have.attr', 'action', '/');
+        cy.get('form.login').find('input[name="email"]').should('exist');
+        cy.get('form.login').find('input[name="password"]').should('exist');
+        cy.get('form.login').find('input[type="submit"]').should('exist');
     })
 })
